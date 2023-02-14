6b71e274dc0303c77085a624a4aac8ba91026efc
Shresth Hooda <34729192+GitShresth@users.noreply.github.com>
2023-02-14 00:52:57 -0800

    Update Remove Ticket from Database.c

--- Remove Ticket from Database.c
+++ Remove Ticket from Database.c
@@ -9,13 +9,9 @@
 #include "parking.h"
 #include "hashdb.h"
 
-#ifndef MYDELTICKET
-
-#else
-
 /*
  * delticket
- *      remove ticket (summons) from the database
+ *      remove ticket from the database
  *      
  *
  * Inputs:
@@ -23,7 +19,7 @@
  *  state   state id string to be found
  *  summ    summon id string to be found
  *
- * returns 0 if ok -1 if not found or error
+ *  returns 0 if ok -1 if not found or error
  */
 int
 delticket(char *plate, char *state, char *summ)
@@ -34,51 +30,55 @@ delticket(char *plate, char *state, char *summ)
 	hashval = hash(plate) % tabsz;	
 	Chain = *(htable+hashval);
 	
-	if (strtosumid(summ, &summid) != 0)
+	if (strtosumid(summ, &summid) != 0) {
         	return -1;
-
+	}
 	struct vehicle *prevV = NULL;
 	while (Chain != NULL) {
 		if (strcmp(plate, Chain->plate) == 0 && strcmp(state, 
-		   Chain->state) == 0) {
+			Chain->state) == 0) {
 			break;
 		}
 		prevV = Chain;
 		Chain = Chain->next;
 	}
-
-	if (Chain == NULL)
+	if (Chain == NULL) {
 		return -1;
+	}
 	
 	struct ticket *tickSearch = Chain->head;    /*current ticket*/
 	struct ticket *prevTick = NULL;		    /*ticket before*/
 	while (tickSearch != NULL) {
-		if (tickSearch->summons == summid)
+		if (tickSearch->summons == summid) {
 			break;
+		}
 		prevTick = tickSearch;
 		tickSearch = tickSearch->next;
 	}
 	if (tickSearch == NULL) {
 		return -1;
 	}
-	unsigned long long fine = fineTab[tickSearch->code].fine; /*fine val*/
+	unsigned long long fine = fineTab[tickSearch->code].fine;	/*fine val*/
 	Chain->tot_fine -= fine;
 	Chain->cnt_ticket--;
-	if (prevTick != NULL) 
+	if (prevTick != NULL) {
 		prevTick->next = tickSearch->next;
-	else
+	}
+	else {
 		Chain->head = tickSearch->next;
+	}
 	free(tickSearch);
 	
 	if (Chain->cnt_ticket == 0) {
-		if (prevV != NULL) 
+		if (prevV != NULL) {
 			prevV->next = Chain->next;
-		else 
+		}
+		else {
 			*(htable+hashval) = Chain->next;
+		}
 		free(Chain->plate);
 		free(Chain->state);
 		free(Chain);
 	}
-    return 0;
+	return 0;
 }
-#endif
